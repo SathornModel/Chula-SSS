ef3223fd1962553a57bbe8443a9e7add3934a4ea
SathornModel <e@talent.in.th>
2018-05-17 06:56:22 +0700

    Add files via upload

--- models/sathorn-evening-caswakul_sumoconf2018/sathorn_wide_run.bat
+++ models/sathorn-evening-caswakul_sumoconf2018/sathorn_wide_run.bat
@@ -1,2 +1,2 @@
-export SUMO_HOME=/usr/local/src/sumo-0.25.0/
-sumo-gui --time-to-teleport 300 --lanechange.allow-swap true --no-internal-links true --ignore-junction-blocker 1 --random true -c sathorn_w.sumo.cfg -a sathon_wide_tls_20160418_edited.add.xml --summary summary.xml
+export SUMO_HOME=/usr/lib/sumo/
+sumo-gui --time-to-teleport 300 --no-internal-links true --ignore-junction-blocker 1 --random true -c sathorn_w.sumo.cfg -a sathon_wide_tls_20160418_edited.add.xml --summary summary.xml
